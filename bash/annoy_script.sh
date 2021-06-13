565be93b0c66bba50ff4e56cd62cc8c6d33de46b
fqqf <77983090+fqqf@users.noreply.github.com>
2021-06-13 23:59:02 +0300

    Fix typo in script appending to crontab file

--- bash/annoy_script.sh
+++ bash/annoy_script.sh
@@ -3,13 +3,14 @@
 
 
 if [ "$1" == "create" ]; then
-	echo "* */3 * * * HI > ~/Desktop/FILE " >> temp
+	echo "* */3 * * * echo "HI" > ~/Desktop/FILE " >> temp
+	echo crontab -l
 	crontab -l > temp
-	cat temp
 	crontab temp
 	rm temp
 	echo "Annoy file was created ;]"
 elif [ "$1" == "remove" ]; then
+	crontab -r
 	echo "Annoy file was removed 0_o"
 else
 	echo "Wrong usage. Try '$0 create/remove'"
