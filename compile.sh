e0576f28ef854f3d281f27f20e5d33d4c490a8f1
fqqf <77983090+fqqf@users.noreply.github.com>
2021-06-14 15:02:20 +0300

    Make bash script remove temp when it's done

--- compile.sh
+++ compile.sh
@@ -3,21 +3,19 @@
 if [ "$1" == "" ]; then
  echo "Usage: $0 <cprogram>"
 else 
- echo "Compiling $1.c"
- gcc -o temp $1.c
- echo "Launching $1.c"
+ echo "Compiling $1"
+ gcc -o temp $1
+ echo "Launching $1"
  ./temp
+ rm temp
 fi
 
  # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
-function del() {
- rm temp.out
-}
-
 function ctrl_c() {
         echo "** Trapped CTRL-C"
+        rm temp
         exit 1
 }
 
